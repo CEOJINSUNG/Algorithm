65b6ddd6e85ece12232d419981c3709cddea6ad2
CEOJINSUNG <jinsung1048@gmail.com>
2021-11-12 17:34:20 +0900

    Create 16236.py

---
+++ graph/16236.py
@@ -0,0 +1,51 @@
+# 공간의 크기 입력받기
+n = int(input())
+
+# 공간 선언
+n_n = []
+
+# 아기 상어 위치 (x, y)와 baby_shark[0]은 아기상어의 크기
+baby_shark = [2]
+
+# 입력받은 물고기와 아기 상어 위치 넣기
+for i in range(n):
+    fish_shark = list(map(int, input().split()))
+
+    # 아기 상어의 위치 파악하기
+    if 9 in fish_shark:
+        baby_shark.append(fish_shark.index(9))
+        baby_shark.append(i)
+    n_n.append(fish_shark)
+
+# bfs 선언
+def bfs(x, y):
+    # 상하좌우에 필요한 좌표
+    dx = [0, 0, 1, -1]
+    dy = [-1, 1, 0, 0]
+
+    # 초기 넓이 선언
+    initial_width = 0
+
+    # 큐 선언
+    q = []
+
+    # 초기 좌표 삽입
+    q.append((x, y))
+
+    # 초기 좌표 방문한 것으로 바꾸고 넓이 더함
+    n_n[y][x] = 1
+    initial_width += 1
+
+    while q:
+        x_, y_ = q.pop(0)
+
+        for i in range(4):
+            new_x_ = x_ + dx[i]
+            new_y_ = y_ + dy[i]
+            if 0 <= new_x_ < n and 0 <= new_y_ < m and n_n[new_y_][new_x_] == 0:
+                n_n[new_y_][new_x_] = 1
+                initial_width += 1
+                q.append((new_x_, new_y_))
+    
+    return initial_width
+
