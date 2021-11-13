0e3ea6aa77d9f2f410925489cd32247d62f8329f
CEOJINSUNG <jinsung1048@gmail.com>
2021-11-13 15:53:02 +0900

    조건 추가식

--- graph/16236.py
+++ graph/16236.py
@@ -17,6 +17,16 @@ for i in range(n):
         baby_shark.append(i)
     n_n.append(fish_shark)
 
+# 갈수 있는 곳인지 아닌지 판단하는 함수
+def can_go_or_not(x, y):
+    # 아기 상어가 공간을 벗어날 경우 False 반환
+    if x >= n or y >= n:
+        return False
+    # 아기상어의 크기가 가고자 하는 크기보다 작을 경우 False
+    if n_n[y][x] > baby_shark[0]:
+        return False
+    # 방문했던 곳이라면 False
+
 # bfs 선언
 def bfs(x, y):
     # 상하좌우에 필요한 좌표
@@ -34,7 +44,6 @@ def bfs(x, y):
 
     # 초기 좌표 방문한 것으로 바꾸고 넓이 더함
     n_n[y][x] = 1
-    initial_width += 1
 
     while q:
         x_, y_ = q.pop(0)
@@ -42,7 +51,7 @@ def bfs(x, y):
         for i in range(4):
             new_x_ = x_ + dx[i]
             new_y_ = y_ + dy[i]
-            if 0 <= new_x_ < n and 0 <= new_y_ < m and n_n[new_y_][new_x_] == 0:
+            if 0 <= new_x_ < n and 0 <= new_y_ < n and n_n[new_y_][new_x_] <= baby_shark[0]:
                 n_n[new_y_][new_x_] = 1
                 initial_width += 1
                 q.append((new_x_, new_y_))
