4747098a8eafaad15f2f82ec9a89d49811bec001
CEOJINSUNG <jinsung1048@gmail.com>
2022-02-02 22:35:22 +0900

    Create 9251.py

---
+++ class4/9251.py
@@ -0,0 +1,13 @@
+A = list(input())
+B = list(input())
+
+lcs = [[0 for _ in range(len(B)+1)] for _ in range(len(A)+1)]
+
+for i in range(1, len(A)+1):
+    for j in range(1, len(B)+1):
+        if A[i-1] == B[j-1]:
+            lcs[i][j] = lcs[i-1][j-1] + 1
+        else:
+            lcs[i][j] = max(lcs[i][j-1], lcs[i-1][j])
+
+print(lcs[len(A)][len(B)])
