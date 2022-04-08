de01860d11c6c380574591bb9af8913ca69f7092
CEOJINSUNG <jinsung1048@gmail.com>
2022-04-08 22:42:33 +0900

    Update 7620.py

--- string/7620.py
+++ string/7620.py
@@ -2,7 +2,7 @@ n = input()
 m = input()
 
 def dist(str1, str2):
-    dp = [[0] for _ in range(len(str2) + 1) for _ in range(len(str1) + 1)]
+    dp = [[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
 
     for i in range(1, len(str1) + 1):
         dp[i][0] = i
@@ -12,10 +12,24 @@ def dist(str1, str2):
     
     for i in range(1, len(str1) + 1):
         for j in range(1, len(str2) + 1):
-            if str[i-1] == str2[j-1]:
+            if str1[i-1] == str2[j-1]:
+                # 두 문자가 같은 경우, 추가 연산이 필요하지 않으므로 (i-1, j-1)에서 가져온다. 'c' 
                 dp[i][j] = dp[i-1][j-1]
+                print('c ' + str2[j-1])
             else:
-                dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[j][i-1]) + 1
+                # 두 문자가 다른 경우, (교체 'm'| 추가 'a' | 삭제 'd') 세 연산 중 cost가 가장 적은 것을 기록한다. 
+                m = dp[i-1][j-1]
+                a = dp[i-1][j]
+                d = dp[i][j-1]
+                dp[i][j] = min(m, a, d) + 1
+                
+                if dp[i][j] == a+1:
+                    print('a ', str1[i-1])
+                elif dp[i][j] == m+1:
+                    print('m '+ str1[i-1])
+                elif dp[i][j] == d+1:
+                    print('d ' + str1[i])
+
     
     return dp[-1][-1]
 
