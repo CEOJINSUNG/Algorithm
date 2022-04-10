abcef2617fe828fa507294dd70477be522ae14a4
CEOJINSUNG <jinsung1048@gmail.com>
2022-04-10 17:05:51 +0900

    Update 7620.py

--- string/7620.py
+++ string/7620.py
@@ -1,36 +1,45 @@
 n = input()
 m = input()
 
-def dist(str1, str2):
-    dp = [[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
+dp = [[0] * (len(n) + 1) for _ in range(len(m) + 1)]
 
-    for i in range(1, len(str1) + 1):
-        dp[i][0] = i
+for i in range(1, len(m) + 1):
+    dp[i][0] = i
     
-    for j in range(1, len(str2) + 1):
-        dp[0][j] = j
+for j in range(1, len(n) + 1):
+    dp[0][j] = j
     
-    for i in range(1, len(str1) + 1):
-        for j in range(1, len(str2) + 1):
-            if str1[i-1] == str2[j-1]:
-                # 두 문자가 같은 경우, 추가 연산이 필요하지 않으므로 (i-1, j-1)에서 가져온다. 'c' 
-                dp[i][j] = dp[i-1][j-1]
-                print('c ' + str2[j-1])
-            else:
-                # 두 문자가 다른 경우, (교체 'm'| 추가 'a' | 삭제 'd') 세 연산 중 cost가 가장 적은 것을 기록한다. 
-                m = dp[i-1][j-1]
-                a = dp[i-1][j]
-                d = dp[i][j-1]
-                dp[i][j] = min(m, a, d) + 1
-                
-                if dp[i][j] == a+1:
-                    print('a ', str1[i-1])
-                elif dp[i][j] == m+1:
-                    print('m '+ str1[i-1])
-                elif dp[i][j] == d+1:
-                    print('d ' + str1[i])
+for i in range(1, len(m) + 1):
+    for j in range(1, len(n) + 1):
+        if m[i-1] == n[j-1]:
+            # 두 문자가 같은 경우, 추가 연산이 필요하지 않으므로 (i-1, j-1)에서 가져온다. 'c' 
+            dp[i][j] = dp[i-1][j-1]
+        else:
+            # 두 문자가 다른 경우, (교체 'm'| 추가 'a' | 삭제 'd') 세 연산 중 cost가 가장 적은 것을 기록한다. 
+            dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+    
+len_n = len(n)
+len_m = len(m)
 
-    
-    return dp[-1][-1]
+trace_array = []
 
-print(dist(n, m))
+while len_n > -1 and len_m > -1:
+    index_pos = min(dp[len_m-1][len_n], dp[len_m-1][len_n-1], dp[len_m][len_n-1])
+    if index_pos == dp[len_m][len_n]:
+        trace_array.append("c " + n[len_n-1])
+        len_n -= 1
+        len_m -= 1
+    else:
+        if index_pos == dp[len_m][len_n-1]:
+            trace_array.append("d " + n[len_n-1])
+            len_n -= 1
+        elif index_pos == dp[len_m-1][len_n-1]:
+            trace_array.append("m " + m[len_m-1])
+            len_n -= 1
+            len_m -= 1
+        else:
+            trace_array.append("a " + m[len_m-1])
+            len_m -= 1
+
+for index in range(len(trace_array)-2, -1, -1):
+    print(trace_array[index])
