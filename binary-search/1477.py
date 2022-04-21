cc6240485bb30809f1ca9adf5a46d37ade9c01f5
CEOJINSUNG <jinsung1048@gmail.com>
2022-04-21 19:17:22 +0900

    Create 1477.py

---
+++ binary-search/1477.py
@@ -0,0 +1,27 @@
+import sys
+
+n, m, l = map(int, sys.stdin.readline().split())
+road = list(map(int, sys.stdin.readline().split()))
+road.append(0)
+road.append(l-1)
+road.sort()
+
+left = 0
+right = road[-1]
+answer = 0
+
+while left <= right:
+    count = 0
+    mid = (left + right)//2
+
+    for i in range(1, len(road)):
+        if (road[i] - road[i-1]) > mid:
+            count += (road[i] - road[i-1] - 1) // mid
+    
+    if count > m:
+        left = mid + 1
+    else:
+        right = mid - 1
+        answer = mid
+
+print(answer)
