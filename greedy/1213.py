5d61b29d0d4729185e9ef63f4f11f5cd938720c7
CEOJINSUNG <jinsung1048@gmail.com>
2022-04-29 20:31:49 +0900

    Update 1213.py

--- greedy/1213.py
+++ greedy/1213.py
@@ -1,38 +1,23 @@
 from collections import defaultdict
 import sys
 
-name = sys.stdin.readline().strip()
-length = len(name)
+d = defaultdict(int)
+s = sys.stdin.readline().strip()
 
-string = defaultdict(int)
+for i in s:
+    d[i] += 1
 
-for i in name:
-    string[i] += 1
+center = ''
+for key, value in d.items():
+    if value%2 == 1:
+        if len(center) > 0:
+            print("I'm Sorry Hansoo")
+            break
+        center = key
 
-sorted_string = sorted(string.items(), key=lambda x: (-x[1], x[0]))
-div = len(name) // 2
-if length % 2 == 1:
-    div += 1
-
-result = ["" for _ in range(length)]
-current = 0
-odd = 0
-while sorted_string:
-    alphabet, number = sorted_string.pop(0)
-    if odd > 1:
-        break
-
-    if number%2 == 1:
-        number -= 1
-        result[div-1] = alphabet
-        odd += 1
-    
-    for _ in range(number//2):
-        result[current] = alphabet
-        result[length-current-1] = alphabet
-        current += 1
-
-if odd > 1:
-    print("I'm Sorry Hansoo")
 else:
-    print("".join(result))
+    ans = ''
+    for key, value in sorted(d.items()):
+        ans += key*(value//2)
+    ans += center + ans[::-1]
+    print(ans)
