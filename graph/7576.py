2da3f72ffcafdfa3e0936eada8449c43bd6fb1b4
CEOJINSUNG <jinsung1048@gmail.com>
2022-04-30 11:56:57 +0900

    Update 7576.py

--- graph/7576.py
+++ graph/7576.py
@@ -19,18 +19,16 @@ answer = 0
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
-def bfs():
-    while position:
-        current_x, current_y = position.popleft()
+while position:
+    current_x, current_y = position.popleft()
 
-        for i in range(4):
-            new_x, new_y = current_x + dx[i], current_y + dy[i]
+    for i in range(4):
+        new_x, new_y = current_x + dx[i], current_y + dy[i]
 
-            if 0 <= new_x < m and 0 <= new_y < n and tomato[new_y][new_x] == 0:
-                tomato[new_y][new_x] = tomato[current_y][current_x] + 1
-                position.append((new_x, new_y))
+        if 0 <= new_x < m and 0 <= new_y < n and tomato[new_y][new_x] == 0:
+            tomato[new_y][new_x] = tomato[current_y][current_x] + 1
+            position.append((new_x, new_y))
 
-bfs()
 for row in tomato:
     if 0 in set(row):
         print(-1)
