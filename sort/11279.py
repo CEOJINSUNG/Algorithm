2d5c21472cf0f29647720d5a856c0d6d41c591f1
CEOJINSUNG <jinsung1048@gmail.com>
2022-06-09 18:42:32 +0900

    Create 11279.py

---
+++ sort/11279.py
@@ -0,0 +1,13 @@
+import sys, heapq
+
+n = int(sys.stdin.readline())
+h = []
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if len(h) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(h)[1])
+    else:
+        heapq.heappush(h, (-x, x))
