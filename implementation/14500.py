b8ebd0cd663df2605d12fd07b71c6ed6f679095b
CEOJINSUNG <jinsung1048@gmail.com>
2022-06-12 17:48:47 +0900

    Update 14500.py

--- implementation/14500.py
+++ implementation/14500.py
@@ -1,42 +1,37 @@
+import sys; input = sys.stdin.readline
+
+def dfs(r, c, idx, total):
+    global ans
+    if ans >= total + max_val * (3 - idx):
+        return
+    if idx == 3:
+        ans = max(ans, total)
+        return
+    else:
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0 <= nr < N and 0 <= nc < M and visit[nr][nc] == 0:
+                if idx == 1:
+                    visit[nr][nc] = 1
+                    dfs(r, c, idx + 1, total + arr[nr][nc])
+                    visit[nr][nc] = 0
+                visit[nr][nc] = 1
+                dfs(nr, nc, idx + 1, total + arr[nr][nc])
+                visit[nr][nc] = 0
+
 N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+visit = [([0] * M) for _ in range(N)]
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
+ans = 0
+max_val = max(map(max, arr))
 
-descarte = []
-for _ in range(N):
-    position = list(map(int, input().split()))
-    descarte.append(position)
+for r in range(N):
+    for c in range(M):
+        visit[r][c] = 1
+        dfs(r, c, 0, arr[r][c])
+        visit[r][c] = 0
 
-score = []
-tetrominos = [
-    [(0, 0), (0, 1), (1, 0), (1, 1)],
-    [(0, 0), (0, 1), (0, 2), (0, 3)],
-    [(0, 0), (1, 0), (2, 0), (3, 0)],
-    [(0, 0), (0, 1), (0, 2), (1, 0)],
-    [(0, 0), (0, 1), (0, 2), (-1, 2)],
-    [(0, 0), (1, 0), (1, 1), (1, 2)],
-    [(0, 0), (0, 1), (0, 2), (1, 2)],
-    [(0, 0), (1, 0), (2, 0), (2, 1)],
-    [(0, 0), (0, 1), (1, 1), (2, 1)],
-    [(0, 0), (0, 1), (1, 0), (2, 0)],
-    [(0, 0), (1, 0), (2, 0), (2, -1)],
-    [(0, 0), (1, 0), (1, 1), (2, 1)],
-    [(0, 0), (0, 1), (1, 0), (-1, 1)],
-    [(0, 0), (0, 1), (1, 0), (1, -1)],
-    [(0, 0), (0, 1), (1, 1), (1, 2)],
-    [(0, 0), (0, 1), (0, 2), (1, 1)],
-    [(0, 0), (1, 0), (1, 1), (1, -1)],
-    [(0, 0), (1, 0), (2, 0), (1, -1)],
-    [(0, 0), (1, 0), (1, 1), (2, 0)]
-]
-
-for i in range(N):
-    for j in range(M):
-        for tetro in tetrominos:
-            amount = 0
-            for x, y in tetro:
-                try:
-                    amount += descarte[i+x][j+y]
-                except IndexError:
-                    break
-            score.append(amount)
-
-print(max(score))
+print(ans)
