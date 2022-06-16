ff35c136fe37c3e6b65bac19b056d764c473e81f
CEOJINSUNG <jinsung1048@gmail.com>
2022-06-16 22:48:40 +0900

    Update 1261.py

--- dijkstra/1261.py
+++ dijkstra/1261.py
@@ -1,36 +1,34 @@
-import heapq
+from collections import deque
 import sys
 input = sys.stdin.readline
 
 m, n = map(int, input().split())
 
-miro = [list(input().strip()) for _ in range(n)]
+miro = [list(map(int, input().strip())) for _ in range(n)]
 
 visited = [[int(1e9)]*m for _ in range(n)]
-def dijkstra(x, y):
-    h = []
-    heapq.heappush(h, (0, x, y))
 
-    dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
+q = deque()
+q.append((0, 0))
+visited[0][0] = 0
 
-    while h:
-        cur_count, cur_x, cur_y = heapq.heappop(h)
-        if cur_x == m-1 and cur_y == n-1:
-            continue
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-        for i in range(4):
-            new_x = cur_x + dx[i]
-            new_y = cur_y + dy[i]
+while q:
+    cur_x, cur_y = q.popleft()
 
-            if 0 <= new_x < m and 0 <= new_y < n:
-                if visited[new_y][new_x] == int(1e9):
-                    if miro[new_y][new_x] == "1":
-                        visited[new_y][new_x] = cur_count + 1
-                        heapq.heappush(h, (cur_count+1, new_x, new_y))
-                    else:
-                        visited[new_y][new_x] = cur_count
-                        heapq.heappush(h, (cur_count, new_x, new_y))
+    for i in range(4):
+        new_x = cur_x + dx[i]
+        new_y = cur_y + dy[i]
 
-dijkstra(0, 0)
+        if 0 <= new_x < m and 0 <= new_y < n:
+            if visited[new_y][new_x] == int(1e9):
+                if miro[new_y][new_x] == 0:
+                    visited[new_y][new_x] = visited[cur_y][cur_x]
+                    q.appendleft((new_x, new_y))
+                else:
+                    visited[new_y][new_x] = visited[cur_y][cur_x] + 1
+                    q.append((new_x, new_y))
+
 print(visited[n-1][m-1])
