1c30bffb08037dabf0e712622d44ab6a61e996d5
CEOJINSUNG <jinsung1048@gmail.com>
2022-06-16 10:49:22 +0900

    Update 10840.py

--- binary-search/10840.py
+++ binary-search/10840.py
@@ -1,52 +1,50 @@
-a = input()
-b = input()
+str1 = input()
+str2 = input()
+mod = 999979
+ans = 0
 
-a_or_b = False
-length = 0
-if len(a) >= len(b):
-    length = len(b)
-    a_or_b = True
-else:
-    length = len(a)
+primes = {}
 
-a_list = []
-for i in range(len(a)):
-    for j in range(i+1, len(a)+1):
-        temp = sorted(list(a[i:j]))
-        if temp not in a_list:
-            a_list.append(temp)
+idx = 0
+for i in range(2,1000):
+    flag = 1
+    for j in range(2,int(i**0.5)):
+        if i % j == 0:
+            flag = 0
+            break
+    if flag == 1:
+        primes[idx] = i
+        idx += 1
 
-b_list = []
-for i in range(len(b)):
-    for j in range(i+1, len(b)+1):
-        temp = sorted(list(b[i:j]))
-        if temp not in b_list:
-            b_list.append(temp)
-answer = 0
-for i in a_list:
-    for j in b_list:
-        if i == j:
-            answer = max(answer, len(i))
+hash_list = [[] for _ in range(mod)]
+    
+for i in range(len(str2)):
+    hash1 = 1
+    hash2 = 1
+    for j in range(i,len(str2)):
+        length = j - i + 1
+        chr_idx = ord(str2[j]) - ord('a')
+        num = primes[chr_idx]
+        hash1 *= num
+        hash1 %= mod
+        hash2 *= primes[chr_idx + 26]
+        hash2 %= mod
+        hash_list[hash1].append((hash2, length))
 
-print(answer)
+for i in range(len(str1)):
+    hash1 = 1
+    hash2 = 1
+    for j in range(i, len(str1)):
+        length = j - i + 1
+        chr_idx = ord(str1[j]) - ord('a')
+        num = primes[chr_idx]
+        hash1 *= num
+        hash1 %= mod
+        hash2 *= primes[chr_idx + 26]
+        hash2 %= mod
+        
+        for k in range(len(hash_list[hash1])):
+            if(hash_list[hash1][k] == (hash2, length)):
+                ans = max(ans, length)
 
-# def compare(short, long):
-#     global answer
-#     short = sorted(list(short))
-#     for i in range(len(long)):
-#         for j in range(i+1, len(long)+1):
-#             temp = sorted(list(long[i:j]))
-
-#             if temp == short:
-#                 answer = max(answer, len(temp))
-
-# length = min(len(a), len(b))
-
-# for left in range(length):
-#     for right in range(left+1, length+1):
-#         if a_or_b:
-#             temp = b[left:right]
-#             compare(temp, a)
-#         else:
-#             temp = a[left:right]
-#             compare(temp, b)
+print(ans)
