64ae49a87567fdad1e7b06e10884701b073970a5
CEOJINSUNG <jinsung1048@gmail.com>
2022-06-23 11:10:52 +0900

    Update 2098.py

--- class5/2098.py
+++ class5/2098.py
@@ -3,38 +3,38 @@
 # from sys import maxsize
 # from itertools import permutations
 
+import sys
+input = sys.stdin.readline
+
 n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
 INF = int(1e9)
-dp = [[INF] * (1 << n) for _ in range(n)]
-
+dp = [[0] * (1 << n-1) for _ in range(n)]
 
 def dfs(x, visited):
-    if visited == (1 << n) - 1:     # 모든 도시를 방문했다면
-        if graph[x][0]:             # 출발점으로 가는 경로가 있을 때
+    if dp[x][visited] != 0:
+        return dp[x][visited]
+    
+    if visited == (1 << (n-1)) - 1:
+        if graph[x][0]: 
             return graph[x][0]
-        else:                       # 출발점으로 가는 경로가 없을 때
+        else:
             return INF
 
-    if dp[x][visited] != INF:       # 이미 최소비용이 계산되어 있다면
-        return dp[x][visited]
-
-    for i in range(1, n):           # 모든 도시를 탐방
-        if not graph[x][i]:         # 가는 경로가 없다면 skip
+    min_route = INF
+    for i in range(1, n):
+        if not graph[x][i]:
             continue
-        if visited & (1 << i):      # 이미 방문한 도시라면 skip
+        if visited & (1 << (i-1)):
             continue
+        dist = dfs(i, visited | (1 << (i-1))) + graph[x][i]
+        if dist < min_route:
+            min_route = dist
+    dp[x][visited] = min_route
+    return min_route
 
-        # 점화식 부분(위 설명 참고)
-        dp[x][visited] = min(dp[x][visited], dfs(i, visited | (1 << i)) + graph[x][i])
-    return dp[x][visited]
-
-
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input().split())))
-
-print(dfs(0, 1))
+print(dfs(0, 0))
 
 
 
