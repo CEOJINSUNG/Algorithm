769d9ae5d8907d38e9908ecc7eaac066c561eb53
CEOJINSUNG <jinsung1048@gmail.com>
2022-06-25 12:19:36 +0900

    처음 해보는 유형 나중에 다시 생각해보기

---
+++ greedy/11877.py
@@ -0,0 +1,27 @@
+n, x = map(int, input().split())
+
+if (n-1)*(n-2) < 2*x:
+    print(-1)
+else:
+    visited = [True]*(n+2)
+    visited[n] = True
+    visited[n-1] = True
+
+    print(n, end=" ")
+
+    mid = x
+    temp = 1
+    while mid >= 0:
+        if (mid >= n - 1 - temp):
+            print(temp, end=" ")
+            mid -= (n - 1 - temp)
+            visited[temp] = True
+            temp += 1
+        else:
+            print(n - 1 - mid, end=" ")
+            visited[n - 1 - mid] = True
+            break
+    print(n-1, end=" ")
+    # for i in range(n, 0, -1):
+    #     if not visited[i]:
+    #         print(i, end=" ")
