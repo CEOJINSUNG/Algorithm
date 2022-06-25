48677d36df6604fc1122f0de2c7267fa737f7454
CEOJINSUNG <jinsung1048@gmail.com>
2022-06-25 13:26:47 +0900

    Create 17425.py

---
+++ cumulative-sum/17425.py
@@ -0,0 +1,18 @@
+import sys
+
+MAX = 1000000
+dp = [0] * (MAX + 1)
+s = [0] * (MAX + 1)
+
+for i in range(1, MAX + 1):
+    j = 1
+    while i * j <= MAX:
+        dp[i * j] += i
+        j += 1
+    s[i] = s[i - 1] + dp[i]
+
+t = int(input())
+
+for _ in range(t):
+    num = int(sys.stdin.readline())
+    print(str(s[num]))
