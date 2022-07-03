8506a47390fb903396bbfe743f9bc0cd1f146ab6
CEOJINSUNG <jinsung1048@gmail.com>
2022-07-03 10:16:30 +0900

    Update 11726.py

--- dynamic-programming/11726.py
+++ dynamic-programming/11726.py
@@ -1,9 +1,7 @@
 n = int(input())
 
-dp = [0 for _ in range(n+1)]
-dp[1], dp[2] = 1, 2
-
+dp = [0, 1, 2]
 for i in range(3, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+    dp.append(dp[i-1] + dp[i-2])
 
 print(dp[n]%10007)
