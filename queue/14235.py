552393acdc57f2bc553a0980c34b102da6080492
CEOJINSUNG <jinsung1048@gmail.com>
2022-08-24 11:51:13 +0900

    Create 14235.py

---
+++ queue/14235.py
@@ -0,0 +1,15 @@
+import heapq
+
+n = int(input())
+gift = []
+for i in range(n):
+    a = list(map(int, input().split()))
+    if a[0] == 0:
+        if len(gift) == 0:
+            print(-1)
+        else:
+            temp = -heapq.heappop(gift)
+            print(temp)
+    else:
+        for j in range(a[0]):
+            heapq.heappush(gift, -a[j+1])
