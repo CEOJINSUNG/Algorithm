1250c75fef7e83f56b7fcfd929dbba57e3ad3e6c
CEOJINSUNG <jinsung1048@gmail.com>
2022-08-30 21:40:30 +0900

    Update 16954.py

--- bfs-dfs/16954.py
+++ bfs-dfs/16954.py
@@ -1,32 +1,24 @@
-from collections import deque
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-miro = [list(input().strip()) for _ in range(8)]
-visited =[[False] * 8 for _ in range(8)]
-
-dx = [0, 0, 1, -1, 1, -1, 1, -1, 0]
-dy = [1, -1, 0, 0, 1, 1, -1, -1, 0]
-
-q = deque()
-q.append((7, 0))
-visited[7][0] = True
-
+miro = [list(input().rstrip()) for _ in range(8)]
+move = [[0, 0], [0, -1], [0, 1], [-1, 0], [1, 0], [-1, -1], [1, -1], [1, 1], [-1, 1]]
 answer = 0
-while q:
-    x, y = q.popleft()
 
-    if miro[x][y] == '#':
-        continue
-
-    for i in range(9):
-        new_x, new_y = x + dx[i], y + dy[i]
-
-        if 0 <= new_x < 8 and 0 <= new_y < 8 and miro[new_x][new_y] == "." and not visited[new_x-1][new_y]:
-            visited[new_x-1][new_y] = True
-            q.append((new_x-1, new_y))
-
-            if new_x == 0:
+queue = deque()
+queue.append((7, 0, 0))
+    
+while queue:
+    x, y, turn = queue.popleft()
+    for dx, dy in move:
+        nx = x + dx
+        ny = y + dy
+            
+        if 0 <= nx < 8 and 0 <= ny < 8 and not miro[nx-turn][ny]=='#' and not miro[nx-turn-1][ny]=='#':
+            if nx-turn < 0:
                 answer = 1
+                break
+            queue.append([nx, ny, turn + 1])
 
 print(answer)
