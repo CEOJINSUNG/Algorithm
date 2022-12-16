e6874923f40b4b2250744aece22f388d1bbdd880
CEOJINSUNG <jinsung1048@gmail.com>
2022-12-16 21:02:04 +0900

    Create Single Number.java

---
+++ leetcode/Single Number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+
+  public int singleNumber(int[] nums) {
+    int sum = 0;
+    ArrayList<Integer> visited = new ArrayList<Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      if (visited.contains(nums[i])) {
+        sum -= nums[i];
+      } else {
+        visited.add(nums[i]);
+        sum += nums[i];
+      }
+    }
+    return sum;
+  }
+}
