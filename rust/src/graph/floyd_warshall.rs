f9b25962b2acf29dec0ba106613af4f407e83afc
CEOJINSUNG <jinsung1048@gmail.com>
2022-12-26 21:51:50 +0900

    Create floyd_warshall.rs

---
+++ rust/src/graph/floyd_warshall.rs
@@ -0,0 +1,61 @@
+use std::collections::BTreeMap;
+use std::ops::Add;
+
+type Graph<V, E> = BTreeMap<V, BTreeMap<V, E>>;
+
+pub fn floyd_warshall<V: Ord + Copy, E: Ord + Copy + Add<Output = E>> (
+    graph: &Graph<V, E>,
+) -> BTreeMap<V, BTreeMap<V, E>> {
+    let mut map: BTreeMap<V, BTreeMap<V, E>> = BTreeMap::new();
+    for (u, edges) in graph.iter() {
+        if !map.contains_key(u) {
+            map.insert(*u, BTreeMap::new());
+        }
+
+        for (v, weight) in edges.iter() {
+            if !map.contains_key(v) {
+                map.insert(*v, BTreeMap::new());
+            }
+
+            map.entry(*u).and_modify(|mp| {
+                mp.insert(*v, *weight);
+            })
+        }
+    }
+
+    let keys = map.keys().copied().collect::<Vec<_>>();
+    for &k in &keys {
+        for &i in &keys {
+            if map[&i].get(&k).is_none() {
+                continue;
+            }
+
+            for &j in &keys {
+                if i == j {
+                    continue;
+                }
+
+                if !map[&k].contains_key(&j) {
+                    continue;
+                }
+
+                let entry_i_j = map[&i].get(&j);
+                let entry_i_k = map[&i][&k];
+                let entry_k_j = map[&k][&j];
+
+                match entry_i_j {
+                    Some(&e) => {
+                        if e > entry_i_k + entry_k_j {
+                            map.entry(i).or_default().insert(j, entry_i_k + entry_k_j);
+                        }
+                    }
+                    None => {
+                        map.entry(i).or_default().insert(j, entry_i_k + entry_k_j);
+                    }
+                };
+            }
+        }
+    }
+
+    map
+}
