fbb063648ae3d407a45b6b6a60eca0d2e6387cad
CEOJINSUNG <jinsung1048@gmail.com>
2022-12-26 17:15:37 +0900

    Create dfs.rs

---
+++ rust/src/graph/dfs.rs
@@ -0,0 +1,69 @@
+use std::collections::HashSet;
+use std::collections::VecDeque;
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+pub struct Vertex(u32);
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+pub struct Edge(u32, u32);
+
+#[derive(Clone)]
+pub struct Graph {
+    #[allow(dead_code)]
+    vertices: Vec<Vertex>,
+    edges: Vec<Edge>
+}
+
+impl Graph {
+    pub fn new(vertices: Vec<Vertex>, edges: Vec<Edge>) -> Self {
+        Graph { vertices, edges }
+    }
+}
+
+impl From<u32> for Vertex {
+    fn from(item: u32) -> Self {
+        Vertex(item)
+    }
+}
+
+impl Vertex {
+    pub fn value(&self) -> u32 {
+        self.0
+    }
+
+    pub fn neighbors(&self, graph: &Graph) -> VecDeque<Vertex> {
+        graph.edges.iter()
+            .filter(|e| e.0 == self.0)
+            .map(|e| e.1.into())
+            .collect()
+    }
+}
+
+impl From<(u32, u32)> for Edge {
+    fn from(item: (u32, u32)) -> Self {
+        Edge(item.0, item.1)
+    }
+}
+
+pub fn dfs(graph: &Graph, root: Vertex, objective: Vertex) -> Option<Vec<u32>> {
+    let mut visited: HashSet<Vertex> = HashSet::new();
+    let mut history: Vec<u32> = Vec::new();
+    let mut queue = VecDeque::new();
+
+    queue.push_back(root);
+
+    while let Some(current_vertex) = queue.pop_front() {
+        history.push(current_vertex.value());
+        
+        if current_vertex == objective {
+            return Some(history);
+        }
+
+        for neighbor in current_vertex.neighbors(graph).into_iter().rev() {
+            if visited.insert(neighbor) {
+                queue.push_front(neighbor);
+            }
+        }
+    }
+    None
+}
